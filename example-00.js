7120939dab939e4997be501e86313cf14443490c
saratrasv <cyrondelahoz@gmail.com>
2024-12-23 13:08:46 -0400

    initial commit

---
+++ example-00.js
@@ -0,0 +1,44 @@
+let person = {
+    name: "cristian"
+}
+
+// console.log(person.name)
+let set = new Set();
+set.add(1)
+set.add({name: "cristian"})
+set.add(person)
+
+set.forEach(element => {
+    console.log(element)
+});
+
+/**
+ * @description this is the implementation of a callbacko
+ */
+
+function principal(callback){
+    let animal = {
+        name: "Sheitan",
+        age: 4,
+        raza: "Pitbull"
+    }
+    callback(animal)
+}
+
+principal((element) => {
+    console.log(element)
+});
+
+
+/**
+ * @description this is the implementation of IIFE(Immediately invoke function expression) 
+ * It helps to create a local scope, avoiding global variable pollution, and enabling the use of closures
+ */
+
+(() => {
+    let userName = "cristian";
+    console.log(userName)
+})();
+
+// let userName = "robert"
+// console.log(userName)
---
+++ example.js
@@ -0,0 +1,21 @@
+// import promptSync from 'prompt-sync';
+
+// let myFirtVariable = 'Hello World'
+// console.log(myFirtVariable)
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i)
+// }
+// console.info('End of the script')
+//  switch (myFirtVariable) {
+//     case 'Hello World':
+//         console.log('first case')
+//         break
+//     default:
+//         console.log('default case')
+//         break
+//  }
+
+// const prompt = promptSync();
+// const name = prompt('Enter your name: ');
+// console.log(`Hello, ${name}!`);
