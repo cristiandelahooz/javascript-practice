68b14b310b6041b8f3f2bbb121497b1f27f12228
saratrasv <cyrondelahoz@gmail.com>
2025-01-06 11:41:09 -0400

    chore: commit save point

---
+++ leetcode/1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.js
@@ -0,0 +1,19 @@
+const IS_EMPTY = '0';
+const IS_NOT_EMPTY = '1';
+
+/**
+ * @param {string} boxes
+ * @return {number[]}
+ */
+const minOperations = (boxes) => {
+  for (let i = 0; i < boxes.length; i++) {
+    if (boxes[i] === IS_EMPTY) console.log(`position: ${i} is empty`);
+    else console.log(`position: ${i} is not empty`);
+  }
+
+  return 0;
+};
+
+minOperations('110');
+
+const number = [1, 3, 4];
