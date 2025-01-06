51c294b3c70eb19b4e9c6a2290de501022864577
saratrasv <cyrondelahoz@gmail.com>
2025-01-05 20:11:50 -0400

    chore: add biome dependency and refactor example script

--- examples/eassy1.js
+++ examples/eassy1.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
+const BUTTOM = 97;
+const TOP = 122;
+const SHIFT_LENGTH = 3;
+
 /**
- * @param {String} s
- * @returns {number}
+ * @param {string} s
+ * @param {number[][]} shifts
+ * @return {string}
  */
-const scoreOfString = (s) => {
-  let sum = 0;
-  for (let i = 0; i < s.length - 1; i++) {
-    sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+shiftingLetters = (s, shifts) => {
+  for (let i = 0; i < shifts.length; i++) {
+    for (let value = 0; value < SHIFT_LENGTH; value++) {}
   }
-
-  return sum;
 };
-
-console.log(scoreOfString('hello'));
